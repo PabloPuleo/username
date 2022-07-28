2c6d03e0b8bf75759064b1e01d2b6684c925b90e
PabloPuleo <108903871+PabloPuleo@users.noreply.github.com>
2022-07-28 19:20:05 -0300

    Add files via upload

---
+++ cumple.js
@@ -0,0 +1,33 @@
+
+
+function cumple(){
+    let mes = document.querySelector("#mes");
+
+if (mes.value == "Enero" || mes.value == "enero") {
+    alert ("Naciste el mismo mes que Foyth, Montiel, Martínez y Julián Álvarez");
+} else if (mes.value == "Febrero" || mes.value == "febrero") {
+    alert ("Naciste el mismo mes que Otamendi, Di María y Papu Gómez");
+} else if (mes.value == "Marzo" || mes.value == "marzo") {
+    alert ("Naciste el mismo mes que Ángel Correa");
+} else if (mes.value == "Abril" || mes.value == "abril") {
+    alert ("Naciste el mismo mes que el Cuti Romero, Molina Lucero, Nico Gonzáles, Guido Rodriguez y Lo Celso");
+} else if (mes.value == "Mayo" || mes.value == "mayo") {
+    alert ("Naciste el mismo mes que Rulli, Musso, Senesi y De Paul");
+} else if (mes.value == "Junio" || mes.value == "junio") {
+    alert ("Naciste el mismo mes que Messi, Pezzella, Pérez y Paredes");
+} else if (mes.value == "Julio" || mes.value == "julio") {
+    alert ("En este mes no nació ningún jugador de la selección");
+} else if (mes.value == "Agosto" || mes.value == "agosto") {
+    alert ("Naciste el mismo mes que Tagliafico, Lautaro Martínez y Joaquín Correa");
+} else if (mes.value == "Septiembre" || mes.value == "septiembre") {
+    alert ("Naciste el mismo mes que el Dibu Martínez");
+} else if (mes.value == "Octubre" || mes.value == "octubre") {
+    alert ("Naciste el mismo mes que Armani, Acuña y Palacios");
+} else if (mes.value == "Noviembre" || mes.value == "noviembre") {
+    alert ("Naciste el mismo mes que Dybala");
+} else if (mes.value == "Diciembre" || mes.value == "diciembre") {
+    alert ("Naciste el mismo mes que Mac Allister");
+} else {
+    alert ("No reconozco ese mes");
+}
+}
